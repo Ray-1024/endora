84bd3f90eae457c08b2b45d39297c62e4a89e11a
Ray_1024 <skovorodnikovda@mail.ru>
2024-12-23 23:19:19 +0300

    Initial commit

---
+++ core/src/ray1024/endora/MainGame.java
@@ -0,0 +1,11 @@
+package ray1024.endora;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainGame extends ApplicationAdapter {
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0, 1);
+    }
+}
