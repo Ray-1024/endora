f8ab01d28475c203245b0e67b3a1696670459808
Ray_1024 <skovorodnikovda@mail.ru>
2026-02-06 13:15:59 +0300

    SFML 2.6.2 init

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Endora");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                {
+                    window.close();
+                }
+            }
+        }
+
+        window.clear(sf::Color(50, 50, 50));
+        window.display();
+    }
+
     return 0;
 }
